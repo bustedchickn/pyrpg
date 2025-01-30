5217d7858ba3d6b028bf59fa427a0d45af49d7c1
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2025-01-29 18:15:51 -0700

    added a testing thing
    added testing and updated game.py

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# Use the hashtag button to make a comment
+escape = ""
+
+
+'''
+Comments help you see what is happening in the code
+This is a multiline comment in python
+'''
+
+# Main game loop
+while(escape != "yes"):
+    print("This is the location you are in.")
+    choice = input("Do you want to go right or left?")
+    if(choice== "right"):
+        print("You see a green goblin.")
+        choice = "left"
+    else:
+        print("You see a lighthouse.")
+
+
+
+    escape = input("Do you want to quit?")
---
+++ testing.py
@@ -0,0 +1,5 @@
+print("Once upon a time...")
+input("press enter to continue")
+
+
+print("\nyou can have it display on this line\nAnd this line\n")
