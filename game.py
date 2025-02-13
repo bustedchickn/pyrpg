2c885e66d0e9a7868545049d4ba027235d2ead76
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2025-02-12 18:38:13 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 # Use the hashtag button to make a comment
 escape = ""
 
@@ -7,16 +8,79 @@ Comments help you see what is happening in the code
 This is a multiline comment in python
 '''
 
+
+class Location():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    def set_connections(self, connections):
+        self.connections = connections
+        
+
+forest = Location("forest","A scary, evil forest.")
+cave = Location("cave","A dark, damp cave.")
+castle = Location("castle","A regal castle.")
+city = Location("city","A busy city.")
+
+forest.set_connections([castle,city,cave])
+castle.set_connections([forest,city])
+city.set_connections([forest,castle])
+cave.set_connections([forest])
+
+for connection in forest.connections:
+    print(connection.description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Dice():
+    def __init__(self, sides):
+        self.number_of_sides = sides
+    def roll(self):
+        print(random.randint(1,self.number_of_sides))
+
+
+D20 = Dice(20)
+D20.roll()
+
+D6 = Dice(6)
+D6.roll()
+    
+
+
+locations = [forest,castle]
+
+for location in locations:
+    input(location.description)
+
+dice_list = []
+for i in range(4,20):
+    die = Dice(i)
+    # die.roll()
+die.roll()
+
+
+
+
 # Main game loop
-while(escape != "yes"):
-    print("This is the location you are in.")
-    choice = input("Do you want to go right or left?")
-    if(choice== "right"):
-        print("You see a green goblin.")
-        choice = "left"
-    else:
-        print("You see a lighthouse.")
+# while(escape != "yes"):
+#     print("This is the location you are in.")
+#     choice = input("Do you want to go right or left?")
+#     if(choice== "right"):
+#         print("You see a green goblin.")
+#         choice = "left"
+#     else:
+#         print("You see a lighthouse.")
 
 
 
-    escape = input("Do you want to quit?")
+#     escape = input("Do you want to quit?")
